eed31807b5281d5eac1cd68aae7723300f9cdf43
andimitre <andi.mitre@nbcuni.com>
2017-10-12 23:49:02 -0400

    initial pages

---
+++ assets/js/login.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("#login").submit(function(e) {
+        console.log("sadasdsa");
+        e.preventDefault();
+        window.location = "mybooks.html";
+    });
+});
---
+++ assets/js/mybooks.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("#index").click(function(e) {
+        console.log("sadasdsa");
+        window.location = "index.html";
+    });
+});
